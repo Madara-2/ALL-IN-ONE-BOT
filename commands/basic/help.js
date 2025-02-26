b874d1606581926091ac912b4ea623d1aaffa1d6
Madara-2 <155267436+Madara-2@users.noreply.github.com>
2025-02-26 11:53:39 +0600

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -134,7 +134,7 @@ module.exports = {
                         `- **Categories Enabled:** ${getEnabledCategories(config.categories).join(', ')}`,
                         `- **Prefix Commands:** ${Object.values(config.excessCommands).some(v => v) ? 'Enabled' : 'Disabled'}`
                     ].join('\n'),
-                    author: { name: 'All In One Bot' }
+                    author: { name: 'Skyper Mod' }
                 });
 
                 // Command Pages for each category
@@ -304,7 +304,7 @@ module.exports = {
                 .setAuthor({
                     name: "Alert!",
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/YAzKkxEFvW"
                 })
                 .setDescription('- This command can only be used through slash command!\n- Please use `/help`')
                 .setTimestamp();
